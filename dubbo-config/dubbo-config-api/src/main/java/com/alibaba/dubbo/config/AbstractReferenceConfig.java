bad210c80dd4efa2d8f8ac51025a6d5a1e874ac6
wp <403704156@qq.com>
2018-12-05 22:57:44 +0800

    dubbo-study

---
+++ dubbo-config/dubbo-config-api/src/main/java/com/alibaba/dubbo/config/AbstractReferenceConfig.java
@@ -0,0 +1,10 @@
+package com.alibaba.dubbo.config;
+
+/**
+ * @author wuping
+ * @date 2018/12/5
+ */
+
+public abstract class AbstractReferenceConfig extends AbstractInterfaceConfig {
+    private static final long serialVersionUID = 257615355315744564L;
+}
